58b469a31302466e83fa989976227617799299b6
Артем Бондар <162103655+artem1dev@users.noreply.github.com>
2024-05-01 14:04:13 +0300

    Added like model

---
+++ backend/src/comments/likecomment.model.js
@@ -0,0 +1,29 @@
+// Import Mongoose
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+// Create a Schema for Comments
+const likeCommentSchema = new Schema({
+    like: {
+        type: Boolean,
+        required: true
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    commentId: {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+        required: true
+    }
+}, {
+    timestamps: true  // Automatically creates createdAt and updatedAt fields
+});
+
+// Create a Comment model
+const LikeComment = mongoose.model("LikeComment", likeCommentSchema);
+
+export default LikeComment;
---
+++ backend/src/posts/likepost.model.js
@@ -0,0 +1,29 @@
+// Import Mongoose
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+// Create a Schema for Comments
+const likePostSchema = new Schema({
+    like: {
+        type: Boolean,
+        required: true
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    postId: {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+        required: true
+    }
+}, {
+    timestamps: true  // Automatically creates createdAt and updatedAt fields
+});
+
+// Create a Comment model
+const LikePost = mongoose.model("LikePost", likePostSchema);
+
+export default LikePost;
